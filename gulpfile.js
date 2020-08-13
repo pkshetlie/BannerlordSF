a37850bd3f7b0c07f7ebeb5fa7591472a433bcc9
Kévin Peron <kevin.peron22@gmail.com>
2020-08-13 23:55:30 +0200

    Ajout Gulp (Terminé) -> 2 commandes : gulp sass / gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 
 // Include plugins (from package.json)
 var plugins = require('gulp-load-plugins')();
+var sass = require('gulp-sass');
 
 //path
 var source = './src'; // dossier de travail
 var destination = './dist';
 
-gulp.task('css', function () {
-    return gulp.src('scss/*.scss')
-    /* ici les plugins Gulp à exécuter */
-    .pipe(gulp.dest('css'));
+gulp.task('sass', function () {
+    return gulp.src('assets/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('assets/scss/*.scss', gulp.series('sass')); 
+    // Other watchers
 });
