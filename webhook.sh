b7548677cae0495e1232d0a9e2f46cb30c93de0c
Pkshetlie <p.pobelle@id2i.net>
2020-10-21 14:23:14 +0200

    merge travail flashical

--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,6 @@
-composer install
+#composer install
 #php bin/console a:i
-yarn install
+#yarn install
 yarn encore production
 php bin/console doctrine:migrations:migrate
 php bin/console c:c --env=prod
