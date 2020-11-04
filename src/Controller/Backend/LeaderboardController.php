8b8cf69a3712f7df391b46745d62e85acdedb71f
Pkshetlie <p.pobelle@id2i.net>
2020-11-04 17:24:40 +0100

    page challenges OK

--- src/Controller/Backend/LeaderboardController.php
+++ src/Controller/Backend/LeaderboardController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin/leaderboard")
+ * @Route("/leaderboard")
  */
 class LeaderboardController extends AbstractController
 {
