60c9e901042890a6bced13510fed14f1773ba5fb
Pkshetlie <p.pobelle@id2i.net>
2020-11-09 00:12:46 +0100

    corrections

--- src/Controller/Backend/CasterController.php
+++ src/Controller/Backend/CasterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Backend;
 
+use App\Entity\Challenge;
 use App\Entity\Participation;
+use App\Repository\ChallengeRepository;
 use App\Repository\ParticipationRepository;
 use App\Service\ChallengeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,14 +20,42 @@ class CasterController extends AbstractController
      * @Route("/", name="caster_index", methods={"GET"})
      * @param ParticipationRepository $participationRepository
      * @param ChallengeService $challengeService
+     * @param ChallengeRepository $challengeRepository
      * @return Response
      */
-    public function index(ParticipationRepository $participationRepository, ChallengeService $challengeService): Response
+    public function index(ParticipationRepository $participationRepository, ChallengeService $challengeService, ChallengeRepository $challengeRepository): Response
     {
+
         $challenge = $challengeService->getRunningChallenge();
+        if($challenge == null) {
+            /** @var Participation[] $participations */
+            $participations = $participationRepository->findByChallenge($challenge);
+
+
+            return $this->render('backend/caster/index.html.twig', [
+                'participations' => $participations,
+                'challenge' => $challenge,
+
+            ]);
+        }else{
+
+            return $this->render('backend/caster/selection_challenge.html.twig', [
+                'challenges' => $challengeRepository->findBy([],["id"]),
+
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/{id}", name="caster_specifique_index", methods={"GET"})
+     * @param ParticipationRepository $participationRepository
+     * @param ChallengeService $challengeService
+     * @return Response
+     */
+    public function indexSpecifique(ParticipationRepository $participationRepository, Challenge $challenge): Response
+    {
         /** @var Participation[] $participations */
         $participations = $participationRepository->findByChallenge($challenge);
-
 
         return $this->render('backend/caster/index.html.twig', [
             'participations' => $participations,
