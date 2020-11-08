e8bcf2b6631a1a054aedf38d4e21118165d4acd7
Pkshetlie <p.pobelle@id2i.net>
2020-11-09 00:17:38 +0100

    corrections

--- src/Controller/Backend/VideoMakerController.php
+++ src/Controller/Backend/VideoMakerController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Backend;
 
 
+use App\Entity\Challenge;
 use App\Entity\Participation;
 use App\Entity\User;
+use App\Repository\ChallengeRepository;
 use App\Repository\ParticipationRepository;
 use App\Repository\RunRepository;
 use App\Service\ChallengeService;
@@ -27,6 +29,7 @@ class VideoMakerController extends AbstractController
      * @param Request $request
      * @param ChallengeService $challengeService
      * @param ParticipationRepository $participationRepository
+     * @param ChallengeRepository $challengeRepository
      * @return Response
      * @throws \TwitchApi\Exceptions\ClientIdRequiredException
      * @throws \TwitchApi\Exceptions\InvalidLimitException
@@ -34,12 +37,69 @@ class VideoMakerController extends AbstractController
      * @throws \TwitchApi\Exceptions\TwitchApiException
      * @throws \TwitchApi\Exceptions\UnsupportedOptionException
      */
-    public function current(Request $request, ChallengeService $challengeService, ParticipationRepository $participationRepository): Response
+    public function current(Request $request, ChallengeService $challengeService, ParticipationRepository $participationRepository, ChallengeRepository $challengeRepository): Response
     {
         $options = [
             'client_id' => 'o7bf3gxsqjjaameb102lbkre19o32d',
         ];
         $challenge = $challengeService->getRunningChallenge();
+
+        if ($challenge != null) {
+
+            $participants = $participationRepository->findByChallenge($challenge);
+            $clips = [];
+            $twitchApi = new TwitchApi($options);
+
+            $datesstart = $challenge->getChallengeDates()->first()->getStartDate();
+            $datesend = $challenge->getChallengeDates()->last()->getEndDate();
+
+            foreach ($participants as $participant) {
+                if ($participant->getUser()->getTwitchID() != null) {
+                    $clips = array_merge($clips, $twitchApi->getTopClips(
+                        $participant->getUser()->getTwitchID(),
+                        null,
+                        "Mount & Blade II: Bannerlord")['clips']
+                    );
+                }
+            }
+
+            foreach ($clips as $i => $clip) {
+                $date = new \DateTime($clip['created_at']);
+                if ($date <= $datesstart || $date >= $datesend) {
+                    unset($clips[$i]);
+                }
+            }
+
+            return $this->render('backend/video_maker/index.html.twig', [
+                'clips' => $clips
+            ]);
+        } else {
+            return $this->render('backend/video_maker/selection_challenge.html.twig', [
+                'challenges' => $challengeRepository->findBy([], ["id"]),
+
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/{id}", name="video_maker_specifique_admin_new", methods={"GET","POST"})
+     * @param Request $request
+     * @param ChallengeService $challengeService
+     * @param ParticipationRepository $participationRepository
+     * @param ChallengeRepository $challengeRepository
+     * @return Response
+     * @throws \TwitchApi\Exceptions\ClientIdRequiredException
+     * @throws \TwitchApi\Exceptions\InvalidLimitException
+     * @throws \TwitchApi\Exceptions\InvalidTypeException
+     * @throws \TwitchApi\Exceptions\TwitchApiException
+     * @throws \TwitchApi\Exceptions\UnsupportedOptionException
+     */
+    public function currentSpecifique(Request $request, Challenge $challenge, ParticipationRepository $participationRepository, ChallengeRepository $challengeRepository): Response
+    {
+        $options = [
+            'client_id' => 'o7bf3gxsqjjaameb102lbkre19o32d',
+        ];
+
         $participants = $participationRepository->findByChallenge($challenge);
         $clips = [];
         $twitchApi = new TwitchApi($options);
@@ -69,4 +129,5 @@ class VideoMakerController extends AbstractController
         ]);
 
     }
+
 }
