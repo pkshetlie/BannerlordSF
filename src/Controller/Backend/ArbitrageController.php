8c2731a27ad18924b43e05fb745480bffaf6bbce
Pkshetlie <p.pobelle@id2i.net>
2020-11-09 00:43:02 +0100

    corrections

--- src/Controller/Backend/ArbitrageController.php
+++ src/Controller/Backend/ArbitrageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Backend;
 
+use App\Entity\Challenge;
+use App\Repository\ChallengeRepository;
 use App\Repository\ParticipationRepository;
 use App\Service\ChallengeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,22 +19,52 @@ class ArbitrageController extends AbstractController
      * @Route("/", name="arbitrage_index", methods={"GET"})
      * @param ParticipationRepository $participationRepository
      * @param ChallengeService $challengeService
+     * @param ChallengeRepository $challengeRepository
      * @return Response
      */
-    public function index(ParticipationRepository $participationRepository, ChallengeService $challengeService): Response
+    public function index(ParticipationRepository $participationRepository, ChallengeService $challengeService, ChallengeRepository $challengeRepository): Response
     {
-        $participations = $participationRepository->findByChallenge($challengeService->getRunningChallenge());
+        $challenge = $challengeService->getRunningChallenge();
+        if ($challenge != null) {
+
+            $participations = $participationRepository->findByChallenge($challenge);
+            $twitch = [];
+            foreach ($participations as $participation) {
+                if ($participation->getUser()->getTwitchID() != null) {
+                    $twitch[] = $participation->getUser()->getTwitchID();
+                }
+            }
+
+            return $this->render('backend/arbitrage/index.html.twig', [
+                'participations' => $participations,
+                'allTwitch' => $twitch
+            ]);
+        } else {
+            return $this->render('backend/arbitrage/selection_challenge.html.twig', [
+                'challenges' => $challengeRepository->findBy([], ["id" => "asc"]),
+
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/{id}", name="arbitrage_specifique_index", methods={"GET"})
+     * @param ParticipationRepository $participationRepository
+     * @param Challenge $challenge
+     * @return Response
+     */
+    public function indexSpecifique(ParticipationRepository $participationRepository, Challenge $challenge): Response
+    {
+        $participations = $participationRepository->findByChallenge($challenge);
         $twitch = [];
         foreach ($participations as $participation) {
             if ($participation->getUser()->getTwitchID() != null) {
                 $twitch[] = $participation->getUser()->getTwitchID();
             }
         }
-
         return $this->render('backend/arbitrage/index.html.twig', [
             'participations' => $participations,
             'allTwitch' => $twitch
-
         ]);
     }
 }
