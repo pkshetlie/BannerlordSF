b355a15e0d7c0553d1c17169a746771a6f5d53fd
Pkshetlie <p.pobelle@id2i.net>
2020-11-09 15:18:41 +0100

    corrections

--- src/Controller/Backend/VideoMakerController.php
+++ src/Controller/Backend/VideoMakerController.php
@@ -125,7 +125,7 @@ class VideoMakerController extends AbstractController
                 $clips = array_merge($clips, $twitchApi->getTopClips(
                     $participant->getUser()->getTwitchID(),
                     null,
-                    self::Game,1000,'month')['clips']
+                    self::Game,1000,'week')['clips']
 
                 );
             }
