592aafa53d3be40ff45b4bcaf85a5bcc08e420a2
Pkshetlie <p.pobelle@id2i.net>
2020-12-02 00:15:00 +0100

    inscription aux challenges

--- src/Service/StatistiquesService.php
+++ src/Service/StatistiquesService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Entity\Challenge;
 use App\Entity\Participation;
+use App\Entity\User;
 use App\Repository\ChallengeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -25,8 +26,16 @@ class StatistiquesService
     {
         return $this->em->getRepository(Challenge::class)->count([]);
     }
+
     public function countParticipations()
     {
         return $this->em->getRepository(Participation::class)->count(["enabled"=>true]);
+    }
+
+    public function countTwitchers()
+    {
+        return count($this->em->getRepository(User::class)->createQueryBuilder('u')->where("u.twitchID is not null")
+            ->getQuery()->getResult());
+
     }
 }
